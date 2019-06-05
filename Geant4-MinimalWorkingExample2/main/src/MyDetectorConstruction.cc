45d47f6d6ea6f0888317da6e2bd69d259ef3eda5
Mariusz Jozef Hoppe <mariuszjozefhoppe@yahoo.com.au>
2019-06-05 14:02:13 +0200

    Fixed code indenting

--- Geant4-MinimalWorkingExample2/main/src/MyDetectorConstruction.cc
+++ Geant4-MinimalWorkingExample2/main/src/MyDetectorConstruction.cc
@@ -34,33 +34,20 @@ void MyDetectorConstruction::DefineMaterials()
 
 G4VPhysicalVolume* MyDetectorConstruction::ConstructDetector()
 {
-	G4bool checkOverlaps = true;
-	G4double opacity = 0.4;
-
-	G4VisAttributes *orange = new G4VisAttributes(G4Colour(1, 0.65, 0, opacity));
-	orange->SetVisibility(true);
-	orange->SetForceWireframe(true);	// Whichever is last overwrites the previous one!
-	orange->SetForceSolid(true);		// Whichever is last overwrites the previous one!
-
-	G4VisAttributes *yellow = new G4VisAttributes(G4Colour(1, 1, 0, opacity));
-	yellow->SetVisibility(true);
-	yellow->SetForceSolid(true);
-	yellow->SetForceWireframe(true);
-
 	solidLab = new G4Box("Lab",          //its name
-						 halfLabSize.x(), halfLabSize.y(), halfLabSize.z());	//its size
+						halfLabSize.x(), halfLabSize.y(), halfLabSize.z());	//its size
 
-	logicalLab = new G4LogicalVolume(solidLab,    //its solid
+	logicalLab = new G4LogicalVolume(solidLab,
 									labMaterial, //its material
 									"Lab");      //its name
 
-	physicalLab = new G4PVPlacement(0,   			//no rotation
-								   G4ThreeVector(), //at (0,0,0)
-								   logicalLab,      //its logical volume
-								   "Lab",           //its name
-								   0, //NULL,        //its mother  volume
-								   false,            //no boolean operation
-								   0);               //copy number
+	physicalLab = new G4PVPlacement(0,   			 //no rotation
+									G4ThreeVector(), //at (0,0,0)
+									logicalLab,      //its logical volume
+									"Lab",           //its name
+									0, //NULL,       //its mother volume
+									false,           //no boolean operation
+									0);              //copy number
 
 	return physicalLab;
 }
